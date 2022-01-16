8754b66f60a14e6c1243b1b4eef690b413aed93f
Juan Patricio Marroquin <review0312oficial@gmail.com>
2022-01-15 20:49:25 -0500

    Añadiendo datos adicionales al componente WeatherDetails

--- src/components/weather-details/weather-details.component.jsx
+++ src/components/weather-details/weather-details.component.jsx
@@ -17,6 +17,55 @@ const WeatherDetails = () => {
                     <div className="place">Lima, PE</div>
                 </div>
             </div>
+
+            <div className="date">{new Date().toLocaleString()}</div>
+
+            <div className="extra-temp">
+                <div className="temp-info-minmax">
+                    <div className="two-sided-section">
+                        <p>
+                            <i className={"wi wi-sunset"}></i>
+                        </p>
+                        <p className="extra-info-leftside">
+                            6:30 PM <br />
+                            Sunset
+                        </p>
+                    </div>
+
+                    <div className="two-sided-section">
+                        <p>
+                            <i className={"wi wi-humidity"}></i>
+                        </p>
+                        <div className="extra-info-leftside">
+                            444 <br/>
+                            Humidity
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="temp-info-minmax">
+                    <div className="two-sided-section">
+                        <p>
+                            <i className={"wi wi-rain"}></i>
+                        </p>
+                        <div className="extra-info-leftside">
+                            444 <br/>
+                            Presure
+                        </div>
+                    </div>
+                    
+                    <div className="two-sided-section">
+                        <p>
+                            <i className={"wi wi-strong-wind"}></i>
+                        </p>
+                        <div className="extra-info-leftside">
+                            444 <br/>
+                            Speed
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </article>
     );
 }
